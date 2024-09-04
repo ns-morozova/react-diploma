10839edc6b8105aba07354e90339f545edc7f983
ns-morozova <ns_morozova@mail.ru>
2024-09-04 20:57:12 +0300

    pages about, contacts

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../../widgets/Header';
 import { Footer } from '../../widgets/Footer';
+
 import styles from './Layout.module.css';
 
 export const Layout = () => {
